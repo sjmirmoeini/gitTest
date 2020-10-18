11f61861862c499a71af9af7711d7bd3221c52a8
sjmirmoeini <48188121+sjmirmoeini@users.noreply.github.com>
2020-10-18 21:49:38 +0330

    pisht

--- test.py
+++ test.py
@@ -3,6 +3,16 @@ x = int(input())
 
 def vaziat(y):
     if y > 10:
-        print('kos')
+        print('are')
     if y <= 10:
         print('naa')
+
+def zoj(a):
+    if a%2==0:
+        print('zoj')
+    else:
+        print('fard')
+
+vaziat(x)
+zoj(x)
+
